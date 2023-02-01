06062575c53bbadf6353c0e1794dabce9f3a246f
AlekseyMegliy <meglij46@gmail.com>
2023-02-02 00:51:59 +0200

    Final

--- src/components/HomeMainComponent.vue
+++ src/components/HomeMainComponent.vue
@@ -1,12 +1,15 @@
 <template>
-    <div class="test">
+    <div class="home-main-block">
         <div class="home-main container-fluid">
             <h1 class=" head">home page</h1>
             <span class="slider-button-left" v-bind:class="{inviz: !shift}"  v-on:click="left"><img src="../assets/Mask.svg"/></span>
             <div  class="row  product-slider">
                 
                 <Product class=" col-12 col-lg-4 single-product"
-                v-touch:swipe.left="right"  v-touch:swipe.bottom="right" v-touch:swipe.right="left"
+                v-touch:swipe.left="right"  
+                v-touch:swipe.bottom="right" 
+                v-touch:swipe.right="left"
+                v-touch:swipe.top="left"
                 v-bind:style="{left: shift +'%'}" 
                 v-for="item in dogs"
                 v-bind:doggy="item"
@@ -29,15 +32,13 @@
                 dogs:[],
                 max:8,
                 shift: 0,
-                shift_size: 100,
-                left_edge: true,
                 right_edge: false,
                 width: window.innerWidth
             }
         },
         methods:{
             right(){
-                if(this.width >991 && Math.floor(this.max/3)>this.shift/(-100)+1){
+                if(this.width >991 && Math.floor(this.max/3) > this.shift/(-100)+1){
                 this.shift -=100
                 } else if(this.width >991 && Math.floor(this.max/3)===this.shift/(-100)+1 && this.max%3===2){
                     this.shift -=66,6666
@@ -56,7 +57,7 @@
                 }
             },
             left(){
-                if(this.width >991 && this.shift%(-100).toFixed(2)===0.00){
+                if(this.width >991 && this.shift%(-100)===0 && this.shift !=0){
                 this.shift +=100
                 this.right_edge = false
                 
@@ -68,7 +69,7 @@
                     this.shift +=33,3333
                     this.right_edge = false
                     
-                }else if(this.width <= 991 && this.shift !=0){
+                }else if(this.width <= 991 && this.shift!=0){
                     this.shift+=100
                     this.right_edge = false
                    
@@ -94,8 +95,8 @@
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap');
-.test{
-    min-height: 100%;
+.home-main-block{
+    min-height: 100vh;
     display: flex;
     flex-direction: column;
     
@@ -106,7 +107,7 @@
     flex-grow: 1;
     flex-direction: column;
     justify-content: center;
-    padding: 20px auto;
+    
 }
 .home-main .head{
     text-transform: capitalize;
@@ -114,17 +115,20 @@
     font-size: 36px;
     line-height: 54px;
     color: black;
+    margin:0 100px;
 }
 .home-main .product-slider{
     display: flex;
     position: relative;
     flex-wrap: nowrap;
     overflow: hidden;
+    margin:0 80px;
 }
 .home-main .product-slider .single-product{
     position: relative;
     z-index: 0;
     transition: all 0.5s ease-in;
+    padding: 0 20px;
 }
 
 .home-main .slider-button-left, .home-main .slider-button-right{
@@ -136,7 +140,7 @@
     width: 68px;
     height: 68px;
     content: " ";
-    top: 50%;
+    top: 45%;
     cursor: pointer;
     margin-top: 0px;
     z-index: 5;
@@ -152,12 +156,12 @@
 
 .home-main .slider-button-left{
     transform: rotate(180deg);
-    left: 5%;
+    left: 4.5%;
     
 }
 
 .home-main .slider-button-right{
-    right: 5%;
+    right: 4.5%;
     
 }
 
@@ -167,29 +171,31 @@
 .home-main .slider-button-left:active, .home-main .slider-button-right:active{
     box-shadow:  black 0px 0px 0px 0px;
 }
-@media all and (max-width:991px ){
-    .home-main .product-slider{
-        width: 93%;
+ @media all and (max-width:1300px ){
+    .home-main .slider-button-left{
+        left: 7%;
     }
-    .home-main{
-    padding: 130px 0; 
+    .home-main .slider-button-right{
+        right: 7%;
     }
+    
 }
-@media all and (max-width:771px ){
-    .home-main .product-slider{
-        width: 96%;
+@media all and (max-width:991px ){
+    .home-main .head{
+        margin: 0 20px;
     }
- 
+    .home-main .product-slider{
+        margin: 0;
+    }
+    .home-main .slider-button-left{
+        left: 1%;
+    }
+    .home-main .slider-button-right{
+        right: 1%;
+    }
+    
 }
 
-@media all and (max-width:450px ){
-    .home-main .product-slider{
-        width: 100%;
-    }
-    .home-main .slider-button-left, .home-main .slider-button-right{
-        top: 45%;
-    }
-}
 .home-main .inviz{
     display: none;
 }
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -23,3 +23,23 @@ export default {
   
 }
 </script>
+
+<style>
+html {
+    height: -webkit-fill-available;
+    
+}
+
+body {
+    height: 100vh;
+    min-height: 100vh;
+    min-height: -webkit-fill-available;
+
+}
+#app{
+    min-height: 100vh;
+}
+.home{
+    min-height: 100vh;
+}
+</style>
